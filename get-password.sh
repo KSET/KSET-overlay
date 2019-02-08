a84ea12cb20dcd84d5c0c491a9dc61334ddb8a77
Josip Igrec <me@allypost.net>
2019-02-08 13:10:02 +0100

    Fixed only showing last word of password

--- get-password.sh
+++ get-password.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-pm2 logs --nostream --out --lines 1 | sed -n 2p | cut -d ' ' -f 1 | xargs cat | grep "Admin password" | tail -n 1 | rev | cut -d ' ' -f 1 | rev
+pm2 logs --nostream --out --lines 1 | sed -n 2p | cut -d ' ' -f 1 | xargs cat | grep "Admin password" | tail -n 1 | cut -d ' ' -f 3-
